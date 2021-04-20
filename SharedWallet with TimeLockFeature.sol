0fee4413e8a5906010b25fd8287a6bc55056a797
AbdulWahab <54874951+AbdulWahab222@users.noreply.github.com>
2021-04-20 12:01:17 +0500

    Create SharedWallet with TimeLockFeature.sol

---
+++ SharedWallet with TimeLockFeature.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.5.0 <0.8.4;
+
+contract SimpleSharedWallet {
+    
+    address public owner;
+    address ContractAddress = address(this);
+    uint creationTime = block.timestamp;
+    uint tillThisTime = block.timestamp + 30 days;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => bool) eligible;
+    
+    
+    function sendMoney() public payable {
+        require( block.timestamp > creationTime + 1 minutes);
+        balances[ContractAddress] += msg.value;
+    }
+    
+    function ContBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function setAllowance(address _addr, uint _amount) public {
+        require(msg.sender == owner, "You are not allowed");
+        eligible[_addr] = true;
+        allowance[ContractAddress][_addr] = _amount;
+
+    }
+    
+    function transferFrom(address payable _spender, uint _amount) public {
+        require(block.timestamp < tillThisTime);
+        require(eligible[msg.sender], "You are not authorized");
+        allowance[ContractAddress][_spender] -= _amount;
+        _spender.transfer(_amount);
+        balances[ContractAddress] -= _amount;
+        balances[_spender] += _amount;
+        
+    }
+    
+    function withDraw(address payable _to, uint _amount) public payable {
+        require(msg.sender == owner);
+        _to.transfer(_amount);
+        balances[ContractAddress] -= _amount;
+    }
+}
