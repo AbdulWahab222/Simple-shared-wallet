9207cda9b097510e66b0420b3442b71d759d863b
AbdulWahab <54874951+AbdulWahab222@users.noreply.github.com>
2021-04-20 20:41:46 +0500

    Update Added TimeLockFeature.sol

--- Added TimeLockFeature.sol
+++ Added TimeLockFeature.sol
@@ -1,20 +1,46 @@
+pragma solidity >=0.5.0 <0.8.4;
 
+contract SimpleSharedWallet {
+    
+    address public owner;
     uint creationTime = block.timestamp;
     uint tillThisTime = block.timestamp + 30 days;
     
+    constructor() {
+        owner = msg.sender;
+    }
     
     function sendMoney() public payable {
-        require( block.timestamp > creationTime + 1 minutes);
-        balances[ContractAddress] += msg.value;
-    }
-    
-    
-    function transferFrom(address payable _spender, uint _amount) public {
-        require(block.timestamp < tillThisTime);
-        require(eligible[msg.sender], "You are not authorized");
-        allowance[ContractAddress][_spender] -= _amount;
-        _spender.transfer(_amount);
-        balances[ContractAddress] -= _amount;
-        balances[_spender] += _amount;
         
     }
+    
+    
+    mapping(address => uint) public allowance;
+    mapping(address => bool) eligible;
+    
+    function addAllowance(address _to, uint _amount) public {
+        require(msg.sender == owner);
+        eligible[_to] = true;
+        allowance[_to] = _amount;
+        
+    }
+    
+    
+    modifier ownerOrAllowed(uint _amount){
+        require(allowance[msg.sender] >= _amount,"You are not owner allowed or Allownce limit is exceeded");
+        _;
+    }
+    
+
+    function transfer(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+        require(block.timestamp < tillThisTime);
+        require(eligible[_to] = true);
+        allowance[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+    
+    function withdraw(address payable _to, uint _amount) public {
+        require(msg.sender == owner);
+    }
+    
+}
