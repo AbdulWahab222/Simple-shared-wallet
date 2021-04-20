60c539f4ce8b03c1754ca96776479c2e890b8634
AbdulWahab <54874951+AbdulWahab222@users.noreply.github.com>
2021-04-20 20:09:25 +0500

    Update simple shared wallet.sol

--- simple shared wallet.sol
+++ simple shared wallet.sol
@@ -9,13 +9,10 @@ contract SimpleSharedWallet {
         owner = msg.sender;
     }
     
-    function sendMoney() public payable {
-        
+    function sendMoney() public payable {       
+    
     }
     
-    function checkbalance() public view returns(uint) {
-        return address(this).balance;
-    }
     
     mapping(address => uint) public allowance;
     mapping(address => bool) eligible;
@@ -28,13 +25,13 @@ contract SimpleSharedWallet {
     }
     
     
-    modifier ownerOrAllowed(uint _amount){
-        require(allowance[msg.sender] >= _amount,"You are not owner allowed or Allownce limit is exceeded");
+    modifier allowedAddress(uint _amount){
+        require(allowance[msg.sender] >= _amount,"You are not allowed or Allownce limit is exceeded");
         _;
     }
     
 
-    function transfer(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+    function transfer(address payable _to, uint _amount) public allowedAddress(_amount){
         require(eligible[_to] = true);
         allowance[msg.sender] -= _amount;
         _to.transfer(_amount);
