8ce801666f868c2d000c31f4fd83265d2b794f1c
AbdulWahab <54874951+AbdulWahab222@users.noreply.github.com>
2021-04-20 20:05:13 +0500

    Update simple shared wallet.sol

--- simple shared wallet.sol
+++ simple shared wallet.sol
@@ -4,44 +4,40 @@ pragma solidity >=0.5.0 <0.8.4;
 contract SimpleSharedWallet {
     
     address public owner;
-    address ContractAddress = address(this);
     
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
     
-    mapping(address => uint) public balances;
-    mapping(address => mapping(address => uint)) public allowance;
-    mapping(address => bool) eligible;
-    
-    
     function sendMoney() public payable {
-        balances[ContractAddress] += msg.value;
-    }
-    
-    function ContBalance() public view returns(uint) {
-        return address(this).balance;
-    }
-    
-    function setAllowance(address _addr, uint _amount) public {
-        require(msg.sender == owner, "You are not allowed");
-        eligible[_addr] = true;
-        allowance[ContractAddress][_addr] = _amount;
-
-    }
-    
-    function transferFrom(address payable _spender, uint _amount) public {
-        require(eligible[msg.sender], "You are not authorized");
-        allowance[ContractAddress][_spender] -= _amount;
-        _spender.transfer(_amount);
-        balances[ContractAddress] -= _amount;
-        balances[_spender] += _amount;
         
     }
     
-    function withDraw(address payable _to, uint _amount) public payable {
-        require(msg.sender == owner);
-        _to.transfer(_amount);
-        balances[ContractAddress] -= _amount;
+    function checkbalance() public view returns(uint) {
+        return address(this).balance;
     }
+    
+    mapping(address => uint) public allowance;
+    mapping(address => bool) eligible;
+    
+    function addAllowance(address _to, uint _amount) public {
+        require(msg.sender == owner);
+        eligible[_to] = true;
+        allowance[_to] = _amount;
+        
+    }
+    
+    
+    modifier ownerOrAllowed(uint _amount){
+        require(allowance[msg.sender] >= _amount,"You are not owner allowed or Allownce limit is exceeded");
+        _;
+    }
+    
+
+    function transfer(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+        require(eligible[_to] = true);
+        allowance[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+    
 }
